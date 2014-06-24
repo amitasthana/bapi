6d94fa0a9ad5d9a031ce9643f38b34ccc85bc911
Amit Asthana <amitasthana.777@gmail.com>
2014-06-24 06:50:01 +0530

    Changes to be committed: 	modified:   application/views/pages/destinations.php 	modified:   shop-homepage.html

--- application/views/pages/destinations.php
+++ application/views/pages/destinations.php
@@ -1,52 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Shop Homepage Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <!-- Add custom CSS here -->
-    <link href="css/shop-homepage.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#home">Start Bootstrap</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="#about">About</a>
-                    </li>
-                    <li><a href="#services">Services</a>
-                    </li>
-                    <li><a href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <div class="container">
-
-        <div class="row">
+     <div class="col-md-12">
+     	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+     		<ol class="carousel-indicators">
+     			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+     			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+     			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+     		</ol>
+     		<div class="carousel-inner">
+     			<div class="item active">
+     				<img class="slide-image" src="http://placehold.it/800x300" alt="">
+     			</div>
+     			<div class="item">
+     				<img class="slide-image" src="http://placehold.it/800x300" alt="">
+     			</div>
+     			<div class="item">
+     				<img class="slide-image" src="http://placehold.it/800x300" alt="">
+     			</div>
+     		</div>
+     		<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+     			<span class="glyphicon glyphicon-chevron-left"></span>
+     		</a>
+     		<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+     			<span class="glyphicon glyphicon-chevron-right"></span>
+     		</a>
+     	</div>
+     </div>
